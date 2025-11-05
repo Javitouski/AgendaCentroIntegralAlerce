7b6cfb4b1c6b6499818ced71a1b833d938ffc6ae
Krahen8 <dariensiefert@gmail.com>
2025-11-05 20:51:07 -0300

    Revert "Calendario al dia"
    This reverts commit c1a25168f23b984a267f2487e553ddf6372ed47a.

--- app/src/main/java/prog/android/centroalr/view/PerfilActivity.java
+++ app/src/main/java/prog/android/centroalr/view/PerfilActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
+
 import prog.android.centroalr.R;
 
 public class PerfilActivity extends AppCompatActivity {
